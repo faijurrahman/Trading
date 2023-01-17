3e11cdf87099ee654c17d487a0b866527bfddb75
faijurrahman <faijur@gmail.com>
2023-01-17 17:05:10 +0000

    Updated the script to use repo: https://github.com/faijurrahman/freqtrade.git

--- FreqTrade/SetupCommand.sh
+++ FreqTrade/SetupCommand.sh
@@ -21,8 +21,9 @@
 # Get the current stable version: sudo dpkg -i google-chrome-stable_current_amd64.deb
 # Fix the package: sudo apt install --fix-broken -y
 # Configure the package: sudo dpkg -i google-chrome-stable_current_amd64.deb
+# To launch, enter: google-chrome
 
-
+# The installation commands are takend from here: https://youtu.be/VHvikJmQrVM
 # Install pre-requisite packages for freqtrade
 sudo apt-get update
 sudo apt-get install python3
@@ -31,20 +32,48 @@ sudo apt install -y python3-venv
 sudo apt install -y python3-dev
 sudo apt install -y python3-pandas
 
+# Configure git
+git init
+git remote -v
+git remote add origin https://github.com/faijurrahman/freqtrade.git
+git config --global user.email "faijur@gmail.com"
+git config --global user.name "Faijur Rahman"
+
 # Download stable source code from github
-git clone https://github.com/freqtrade/freqtrade.git
-cd freqtrade/
-git checkout stable
+# git clone https://github.com/freqtrade/freqtrade.git (Original)
+#cd freqtrade
+#git checkout stable
+git clone https://github.com/faijurrahman/freqtrade.git
+cd freqtrade
+
 
 # Setup the freqtrade binaries
 ./setup.sh -i
 
-# Activate venv
+# Activate venv. Use this command every time you restart Ubuntu
 source ./.env/bin/activate
+freqtrade --help
 
-# Create user_data and config.json
+# Create user_data & config.json and then update followings:
 freqtrade create-userdir --userdir user_data
 freqtrade new-config --config config.json
+# Update config.json: exchange->pair_whitelist: ["BTC/USDT", "ETH/USDT"]
+# Update config.json: pairlists->method: "StaticPairList",
+# Please follow the video tutorial for more details: https://youtu.be/VHvikJmQrVM
+git add -f config.json
+git commit -m "Adding default config.json"
+git pull
+git push
 
-# Download data
-freqtrade download-data --config config.json --days 999 -t 5m 15m 30m 1h
+
+# Download data with the following command
+freqtrade list-timeframes
+freqtrade download-data --config config.json --days 999 -t 5m 15m 30m 1h 2h 4h 1d 1w
+# The data will be in user_dat/data/binance folder
+freqtrade list-data
+
+
+# Backtesting the data
+freqtrade backtesting --config config.json --strategy SampleStrategy
+freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001
+freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001 --timeframe=4h
