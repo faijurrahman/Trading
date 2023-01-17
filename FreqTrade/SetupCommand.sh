2e7633d7ca27d93c0bd258aff36b38bbd7d5e0ed
faijurrahman <faijur@gmail.com>
2023-01-17 13:26:51 +0000

    Added wsl gui application installation command

--- FreqTrade/SetupCommand.sh
+++ FreqTrade/SetupCommand.sh
@@ -1,6 +1,29 @@
-!/bin/bash
+#!/bin/bash
 
-# Install pre-requisite packages
+# Please following this guide to prepare Ubuntu in windows WSL
+# Guide: https://learn.microsoft.com/en-us/windows/wsl/tutorials/gui-apps
+
+# Step1: Related commands in Power shell
+# wsl --update
+# wsl --shutdown
+
+# Step2: Then ubuntu commands to install all necessary packages
+# sudo apt update
+# sudo apt install gedit -y
+# sudo apt install gimp -y
+# sudo apt install nautilus -y
+# sudo apt install x11-apps -y
+# sudo apt install geany -y
+
+# Step 3: To install the Google Chrome for Linux:
+# Change directories into the temp folder: cd /tmp
+# Use wget to download it: sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Get the current stable version: sudo dpkg -i google-chrome-stable_current_amd64.deb
+# Fix the package: sudo apt install --fix-broken -y
+# Configure the package: sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+
+# Install pre-requisite packages for freqtrade
 sudo apt-get update
 sudo apt-get install python3
 sudo apt install -y python3-pip
